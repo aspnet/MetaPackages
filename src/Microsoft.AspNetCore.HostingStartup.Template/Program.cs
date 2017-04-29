3e9b57f52d89c6b286794c9ddb031b451345a1b2
Pavel Krymets <pavel@krymets.com>
2017-04-28 17:01:07 -0700

    Build deps files as part of MetaPackages

---
+++ src/Microsoft.AspNetCore.HostingStartup.Template/Program.cs
@@ -0,0 +1,4 @@
+public class Program
+{
+    public static void Main() { }
+}
---
+++ src/TrimDeps/Program.cs
@@ -0,0 +1,58 @@
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ChangeEntryPointLibraryName(args[0], null);
+    }
+
+    private static void ChangeEntryPointLibraryName(string depsFile, string newName)
+    {
+        JToken deps;
+        using (var file = File.OpenText(depsFile))
+        using (JsonTextReader reader = new JsonTextReader(file))
+        {
+            deps = JObject.ReadFrom(reader);
+        }
+
+        string version = null;
+        foreach (JProperty target in deps["targets"])
+        {
+            var targetLibrary = target.Value.Children<JProperty>().FirstOrDefault();
+            if (targetLibrary == null)
+            {
+                continue;
+            }
+            version = targetLibrary.Name.Substring(targetLibrary.Name.IndexOf('/') + 1);
+            if (newName == null)
+            {
+                targetLibrary.Remove();
+            }
+            else
+            {
+                targetLibrary.Replace(new JProperty(newName + '/' + version, targetLibrary.Value));
+            }
+        }
+        if (version != null)
+        {
+            var library = deps["libraries"].Children<JProperty>().First();
+            if (newName == null)
+            {
+                library.Remove();
+            }
+            else
+            {
+                library.Replace(new JProperty(newName + '/' + version, library.Value));
+            }
+            using (var file = File.CreateText(depsFile))
+            using (var writer = new JsonTextWriter(file) { Formatting = Formatting.Indented })
+            {
+                deps.WriteTo(writer);
+            }
+        }
+    }
+}
