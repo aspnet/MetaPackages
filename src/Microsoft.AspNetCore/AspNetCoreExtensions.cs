cafd1b5f5cd5eec70faec150c0d5cd6c872a5328
Hao Kung <haok@microsoft.com>
2017-05-22 16:24:29 -0700

    Slim tests down

--- src/Microsoft.AspNetCore/AspNetCoreExtensions.cs
+++ src/Microsoft.AspNetCore/AspNetCoreExtensions.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Options.Infrastructure;
 
 namespace Microsoft.AspNetCore
--- test/Microsoft.AspNetCore.All.Tests/ConfigurationTests.cs
+++ test/Microsoft.AspNetCore.All.Tests/ConfigurationTests.cs
@@ -2,28 +2,34 @@
 
 using System;
 using System.Collections.Generic;
-using Microsoft.AspNetCore.Authentication.Facebook;
-using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
-using Microsoft.AspNetCore.Identity.Service.IntegratedWebClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Options.Infrastructure;
 using Xunit;
 
 namespace Microsoft.AspNetCore.Tests
 {
     public class ConfigurationTests
     {
-        private const string AuthenticationSection = "Microsoft:AspNetCore:Authentication:";
-        private const string AuthenticationSchemesSection = "Microsoft:AspNetCore:Authentication:Schemes:";
+        private class TestOptions
+        {
+              public string Message { get; set; }
+        }
+      
+        private class ConfigureTestDefault : ConfigureDefaultOptions<TestOptions>
+        {
+            public ConfigureTestDefault(IConfiguration config) :
+                base(options => config.GetSection("Test").Bind(options))
+            { }
+        }
 
         [Fact]
-        public void IntegratedWebClientCanBindAgainstDefaultConfig()
+        public void ConfigureAspNetCoreDefaultsEnablesBindAgainstDefaultConfig()
         {
             var dic = new Dictionary<string, string>
             {
-                { AuthenticationSection + "IdentityService:ClientId", "<id>"},
-                { AuthenticationSection + "IdentityService:TokenRedirectUrn", "urn:self:aspnet:identity:integrated"},
+                { "Test:Message", "yadayada"}
             };
             var configurationBuilder = new ConfigurationBuilder();
             configurationBuilder.AddInMemoryCollection(dic);
@@ -31,93 +37,33 @@ namespace Microsoft.AspNetCore.Tests
             var services = new ServiceCollection()
                 .AddSingleton<IConfiguration>(config)
                 .AddOptions()
+                .AddTransient<ConfigureDefaultOptions<TestOptions>, ConfigureTestDefault>()
                 .ConfigureAspNetCoreDefaults();
             var sp = services.BuildServiceProvider();
 
-            var options = sp.GetRequiredService<IOptions<IntegratedWebClientOptions>>().Value;
-            Assert.Equal("<id>", options.ClientId);
-            Assert.Equal("urn:self:aspnet:identity:integrated", options.TokenRedirectUrn);
+            var options = sp.GetRequiredService<IOptions<TestOptions>>().Value;
+            Assert.Equal("yadayada", options.Message);
         }
 
         [Fact]
-        public void FacebookCanBindAgainstDefaultConfig()
+        public void DefaultConfigIgnoredWithoutConfigureAspNetCoreDefaults()
         {
             var dic = new Dictionary<string, string>
             {
-                {AuthenticationSchemesSection + "Facebook:AppId", "<id>"},
-                {AuthenticationSchemesSection + "Facebook:AppSecret", "<secret>"},
-                {AuthenticationSchemesSection + "Facebook:AuthorizationEndpoint", "<authEndpoint>"},
-                {AuthenticationSchemesSection + "Facebook:BackchannelTimeout", "0.0:0:30"},
-                {AuthenticationSchemesSection + "Facebook:ClaimsIssuer", "<issuer>"},
-                {AuthenticationSchemesSection + "Facebook:RemoteAuthenticationTimeout", "0.0:0:30"},
-                {AuthenticationSchemesSection + "Facebook:SaveTokens", "true"},
-                {AuthenticationSchemesSection + "Facebook:SendAppSecretProof", "true"},
-                {AuthenticationSchemesSection + "Facebook:SignInScheme", "<signIn>"},
-                {AuthenticationSchemesSection + "Facebook:TokenEndpoint", "<tokenEndpoint>"},
-                {AuthenticationSchemesSection + "Facebook:UserInformationEndpoint", "<userEndpoint>"},
+                { "Test:Message", "yadayada"}
             };
             var configurationBuilder = new ConfigurationBuilder();
             configurationBuilder.AddInMemoryCollection(dic);
             var config = configurationBuilder.Build();
             var services = new ServiceCollection()
                 .AddSingleton<IConfiguration>(config)
-                .ConfigureAspNetCoreDefaults()
-                .AddFacebookAuthentication();
+                .AddOptions()
+                .AddTransient<ConfigureDefaultOptions<TestOptions>, ConfigureTestDefault>();
             var sp = services.BuildServiceProvider();
 
-            var options = sp.GetRequiredService<IOptionsSnapshot<FacebookOptions>>().Get(FacebookDefaults.AuthenticationScheme);
-            Assert.Equal("<id>", options.AppId);
-            Assert.Equal("<secret>", options.AppSecret);
-            Assert.Equal("<authEndpoint>", options.AuthorizationEndpoint);
-            Assert.Equal(new TimeSpan(0, 0, 0, 30), options.BackchannelTimeout);
-            Assert.Equal("<issuer>", options.ClaimsIssuer);
-            Assert.Equal("<id>", options.ClientId);
-            Assert.Equal("<secret>", options.ClientSecret);
-            Assert.Equal(new TimeSpan(0, 0, 0, 30), options.RemoteAuthenticationTimeout);
-            Assert.True(options.SaveTokens);
-            Assert.True(options.SendAppSecretProof);
-            Assert.Equal("<signIn>", options.SignInScheme);
-            Assert.Equal("<tokenEndpoint>", options.TokenEndpoint);
-            Assert.Equal("<userEndpoint>", options.UserInformationEndpoint);
+            var options = sp.GetRequiredService<IOptions<TestOptions>>().Value;
+            Assert.Null(options.Message);
         }
 
-        [Fact]
-        public void MicrosoftAccountCanBindAgainstDefaultConfig()
-        {
-            var dic = new Dictionary<string, string>
-            {
-                { AuthenticationSchemesSection + "Microsoft:ClientId", "<id>"},
-                { AuthenticationSchemesSection + "Microsoft:ClientSecret", "<secret>"},
-                { AuthenticationSchemesSection + "Microsoft:AuthorizationEndpoint", "<authEndpoint>"},
-                { AuthenticationSchemesSection + "Microsoft:BackchannelTimeout", "0.0:0:30"},
-                { AuthenticationSchemesSection + "Microsoft:ClaimsIssuer", "<issuer>"},
-                { AuthenticationSchemesSection + "Microsoft:RemoteAuthenticationTimeout", "0.0:0:30"},
-                { AuthenticationSchemesSection + "Microsoft:SaveTokens", "true"},
-                { AuthenticationSchemesSection + "Microsoft:SendAppSecretProof", "true"},
-                { AuthenticationSchemesSection + "Microsoft:SignInScheme", "<signIn>"},
-                { AuthenticationSchemesSection + "Microsoft:TokenEndpoint", "<tokenEndpoint>"},
-                { AuthenticationSchemesSection + "Microsoft:UserInformationEndpoint", "<userEndpoint>"},
-            };
-            var configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.AddInMemoryCollection(dic);
-            var config = configurationBuilder.Build();
-            var services = new ServiceCollection()
-                .AddSingleton<IConfiguration>(config)
-                .ConfigureAspNetCoreDefaults()
-                .AddMicrosoftAccountAuthentication();
-            var sp = services.BuildServiceProvider();
-
-            var options = sp.GetRequiredService<IOptionsSnapshot<MicrosoftAccountOptions>>().Get(MicrosoftAccountDefaults.AuthenticationScheme);
-            Assert.Equal("<authEndpoint>", options.AuthorizationEndpoint);
-            Assert.Equal(new TimeSpan(0, 0, 0, 30), options.BackchannelTimeout);
-            Assert.Equal("<issuer>", options.ClaimsIssuer);
-            Assert.Equal("<id>", options.ClientId);
-            Assert.Equal("<secret>", options.ClientSecret);
-            Assert.Equal(new TimeSpan(0, 0, 0, 30), options.RemoteAuthenticationTimeout);
-            Assert.True(options.SaveTokens);
-            Assert.Equal("<signIn>", options.SignInScheme);
-            Assert.Equal("<tokenEndpoint>", options.TokenEndpoint);
-            Assert.Equal("<userEndpoint>", options.UserInformationEndpoint);
-        }
     }
 }
